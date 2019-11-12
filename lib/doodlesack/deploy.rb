15d46d972a27f23e05621bd5c73c51877bf860c3
Nick Pachulski <nick@pachulski.me>
2019-11-12 02:36:16 -0500

    wip

--- lib/doodlesack/deploy.rb
+++ lib/doodlesack/deploy.rb
@@ -1,35 +1,36 @@
 class Doodlesack
   class Deploy
-    OVER_THE_AIR_VERSION_FILE_PATH = "./OverTheAirVersion.ts"
+    VERSION_NUMBER_FILE = "./OverTheAirVersion.ts"
 
     def self.run
       new.run
     end
 
     def run
-      incremented_over_the_air_version_number = \
-        current_over_the_air_version_number + 1
-      write_version(incremented_over_the_air_version_number)
+      incremented_version_number = current_version_number + 1
+      update_version_number(incremented_version_number)
     end
 
-    def current_over_the_air_version_number
-      File.read(OVER_THE_AIR_VERSION_FILE_PATH)
+    private
+
+    def current_version_number
+      File.read(VERSION_NUMBER_FILE)
         .split
         .detect { |word| /\A\d+\z/ === word }.to_i
     end
 
-    def write_version(version)
+    def update_version_number(version_number)
       create_new_or_overwrite_existing = "w+"
-      over_the_air_version_file = File.open(
-        OVER_THE_AIR_VERSION_FILE_PATH,
+      version_number_file = File.open(
+        VERSION_NUMBER_FILE,
         create_new_or_overwrite_existing,
       )
-      over_the_air_version_file.write <<~END_OF_STRING
-        const OverTheAirVersionNumber = #{version}
+      version_number_file.write <<~END_OF_STRING
+        const OverTheAirVersionNumber = #{version_number}
 
         export default OverTheAirVersionNumber
       END_OF_STRING
-      over_the_air_version_file.close
+      version_number_file.close
     end
   end
 end
--- spec/doodlesack_deploy_spec.rb
+++ spec/doodlesack_deploy_spec.rb
@@ -1,28 +1,28 @@
 require "spec_helper"
 
 describe Doodlesack::Deploy do
-  OVER_THE_AIR_VERSION_FILE_PATH = "OverTheAirVersion.ts"
+  VERSION_NUMBER_FILE = "OverTheAirVersion.ts"
 
   describe "self#run" do
     it "increments the over-the-air version number from 0 to 1" do
       create_new_or_overwrite_existing = "w+"
-      over_the_air_version_file = File.open(
-        OVER_THE_AIR_VERSION_FILE_PATH,
+      version_number_file = File.open(
+        VERSION_NUMBER_FILE,
         create_new_or_overwrite_existing,
       )
-      over_the_air_version_file.write <<~END_OF_STRING
+      version_number_file.write <<~END_OF_STRING
         const OverTheAirVersionNumber = 0
 
         export default OverTheAirVersionNumber
       END_OF_STRING
-      over_the_air_version_file.close
+      version_number_file.close
 
       Doodlesack::Deploy.run
 
-      updated_over_the_air_version_file_content = \
-        File.read(OVER_THE_AIR_VERSION_FILE_PATH)
+      updated_version_number_file_content = \
+        File.read(VERSION_NUMBER_FILE)
 
-      expect(updated_over_the_air_version_file_content).to eq <<~END_OF_STRING
+      expect(updated_version_number_file_content).to eq <<~END_OF_STRING
         const OverTheAirVersionNumber = 1
 
         export default OverTheAirVersionNumber
@@ -31,23 +31,23 @@ describe Doodlesack::Deploy do
 
     it "increments the over-the-air version number from 1 to 2" do
       create_new_or_overwrite_existing = "w+"
-      over_the_air_version_file = File.open(
-        OVER_THE_AIR_VERSION_FILE_PATH,
+      version_number_file = File.open(
+        VERSION_NUMBER_FILE,
         create_new_or_overwrite_existing,
       )
-      over_the_air_version_file.write <<~END_OF_STRING
+      version_number_file.write <<~END_OF_STRING
         const OverTheAirVersionNumber = 1
 
         export default OverTheAirVersionNumber
       END_OF_STRING
-      over_the_air_version_file.close
+      version_number_file.close
 
       Doodlesack::Deploy.run
 
-      updated_over_the_air_version_file_content = \
-        File.read(OVER_THE_AIR_VERSION_FILE_PATH)
+      updated_version_number_file_content = \
+        File.read(VERSION_NUMBER_FILE)
 
-      expect(updated_over_the_air_version_file_content).to eq <<~END_OF_STRING
+      expect(updated_version_number_file_content).to eq <<~END_OF_STRING
         const OverTheAirVersionNumber = 2
 
         export default OverTheAirVersionNumber
