8cdd2d2ee19c021d237b0952ce148b0caed8df3e
Nick Pachulski <nick@pachulski.me>
2019-11-12 04:59:29 -0500

    wip

--- lib/doodlesack/deploy.rb
+++ lib/doodlesack/deploy.rb
@@ -1,14 +1,22 @@
 class Doodlesack
   class Deploy
-    VERSION_NUMBER_FILE = "./OverTheAirVersion.ts"
+    VERSION_NUMBER_FILE = "OverTheAirVersion.ts"
 
     def self.run
       new.run
     end
 
+    def initialize
+      unless File.exist?(VERSION_NUMBER_FILE)
+        create_version_number_file
+      end
+    end
+
     def run
       increment_version_number
       undo_version_number_change unless system("expo publish")
+      `git add #{VERSION_NUMBER_FILE}`
+      `git commit -m 'Increment version number for over the air deploy'`
     end
 
     private
@@ -30,6 +38,20 @@ class Doodlesack
 
     def undo_version_number_change
       update_version_number(version_number)
+    end
+
+    def create_version_number_file
+      create_new_or_overwrite_existing = "w+"
+      version_number_file = File.open(
+        VERSION_NUMBER_FILE,
+        create_new_or_overwrite_existing,
+      )
+      version_number_file.write <<~END_OF_STRING
+        const OverTheAirVersionNumber = 0
+
+        export default OverTheAirVersionNumber
+      END_OF_STRING
+      version_number_file.close
     end
 
     def update_version_number(version_number)
--- spec/doodlesack_deploy_spec.rb
+++ spec/doodlesack_deploy_spec.rb
@@ -4,6 +4,25 @@ describe Doodlesack::Deploy do
   VERSION_NUMBER_FILE = "OverTheAirVersion.ts"
 
   describe "self#run" do
+    context "the version number file does not exist" do
+      it "creates the version number file for version: 1" do
+        stub_successful_expo_publish
+
+        Doodlesack::Deploy.run
+
+        updated_version_number_file_content = \
+          File.read(VERSION_NUMBER_FILE)
+
+        expect(updated_version_number_file_content).to eq <<~END_OF_STRING
+          const OverTheAirVersionNumber = 1
+
+          export default OverTheAirVersionNumber
+        END_OF_STRING
+
+        File.delete(VERSION_NUMBER_FILE)
+      end
+    end
+
     it "increments the over-the-air version number from 0 to 1" do
       create_new_or_overwrite_existing = "w+"
       version_number_file = File.open(
@@ -107,11 +126,28 @@ describe Doodlesack::Deploy do
         File.delete(VERSION_NUMBER_FILE)
       end
     end
+
+    it "commits the version number increment to git" do
+      deploy_instance = Doodlesack::Deploy.new
+      allow(Doodlesack::Deploy).to receive(:new).and_return(deploy_instance)
+      allow(deploy_instance).to receive(:system).with("expo publish")
+        .and_return(true)
+      allow(deploy_instance).to receive(:`)
+
+      Doodlesack::Deploy.run
+
+      expect(deploy_instance).to have_received(:`).with("git add #{VERSION_NUMBER_FILE}")
+      expect(deploy_instance).to have_received(:`)
+        .with("git commit -m 'Increment version number for over the air deploy'")
+
+      File.delete(VERSION_NUMBER_FILE)
+    end
   end
 
   def stub_successful_expo_publish
     deploy_instance = Doodlesack::Deploy.new
     allow(Doodlesack::Deploy).to receive(:new).and_return(deploy_instance)
     allow(deploy_instance).to receive(:system).with("expo publish").and_return(true)
+    allow(deploy_instance).to receive(:`)
   end
 end
